6291c3522514da535e91bd63e55014add08f06c3
Thomas Gomez <thomaseg2016@gmail,com>
2026-04-18 16:45:39 -0300

    Busqueda por ID en branch buscar-nombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -6,12 +6,14 @@
 
     //---FUNCIONES---
     void mostrarPersonas(char *V[], int n);
+    void buscarNombre(char *V[], int n, int id );
 
 
 int main(){
 
     char *V [N];
     char buffer[100];
+    int id;
 
     for (int i = 0; i < N; i++)
     {
@@ -27,21 +29,39 @@ int main(){
     printf("\nLista de nombres: \n");
     mostrarPersonas(V, N);
 
+    //BUSQUEDA POR ID
+    printf("\Ningrese un ID (0 a %d)", N - 1);
+    scanf("%d", &id);
+    
+
+    //LLAMO A LA FUNCION Y LE PASO LOS PARAMETROS
+    buscarNombre(V, N, id);
+
     //LIBERO LA MEMORIA
     for (int i = 0; i < N; i++)
     {
         free(V[i]);
     }
     
+
     return 0;
 }
 
 //FUNCIONES
     void mostrarPersonas(char *V[], int n){
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < n; i++)
         {
             printf("%s\n", V[i]);
         }
         
 
+    }
+
+    void buscarNombre(char *V[], int n, int id ){
+        if (id >= 0 && id < n)
+        {
+            printf("Nombre buscado: %s\n", V[id]);
+        }else{
+            printf("No se encontro el valor buscado.\n");
+        }
     }
